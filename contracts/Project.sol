d212a2ea6b1f33086c6df6340deae4924d065212
onechaiev <a4455395@gmail.com>
2018-08-10 12:07:52 +1200

    deleted extra functions, changed architecture

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -2,7 +2,6 @@ pragma solidity ^0.4.24;
 
 contract ParticipantManager {
     mapping(address => bool) public participants;
-    address[] public participantArray;
     uint public participantAmount;
     ParticipantRequest[] public participantRequests;
 
@@ -31,19 +30,11 @@ contract ParticipantManager {
     function addParticipant(address _participant) internal {
         participants[_participant] = true;
         participantAmount++;
-        participantArray.push(_participant);
     }
 
     function deleteParticipant(address _participant) internal {
         participants[_participant] = false;
         participantAmount--;
-
-        for(uint i = 0; i < participantArray.length; i++) {
-            if(participantArray[i] == _participant) {
-                removeParticipantInArray(i);
-                break;
-            }
-        }
     }
 
     function createPrticipantRequest(ManageParticipantFunction func, address _address)
@@ -77,24 +68,13 @@ contract ParticipantManager {
         request.approvalAmount++;
         request.voted[msg.sender] = true;
     }
-
-    function removeParticipantInArray(uint index) internal {
-        if (index >= participantArray.length) {
-            return;
-        }
-
-        for (uint i = index; i < participantArray.length - 1; i++) {
-            participantArray[i] = participantArray[i+1];
-        }
-        participantArray.length--;
-    }
 }
 
 contract Project is ParticipantManager {
 
     address public productOwner;
     Sprint[] public sprints;
-    SetShareRequest[] public shareRequests;
+    ShareRequest[] public shareRequests;
 
     constructor(address _productOwner) public {
         productOwner = _productOwner;
@@ -118,14 +98,11 @@ contract Project is ParticipantManager {
         uint reward;
     }
 
-    struct ParticipantShare {
-        address participant;
-        uint share;
-    }
-
-    struct SetShareRequest {
+    //for every share holder it is his share with the same index
+    struct ShareRequest {
         uint sprintIndex;
-        ParticipantShare[] participantShares;
+        address[] shareHolders;
+        uint[] shares;
         mapping(address => bool) approved;
         uint approvalAmount;
         bool finalized;
@@ -143,7 +120,7 @@ contract Project is ParticipantManager {
     function createSprint(string name, uint reward) public onlyClient {
         require(address(this).balance >= reward, "Unsufficient funds");
 
-        Sprint memory newSprintMemory = Sprint({
+        Sprint memory newSprint = Sprint({
             name: name,
             createdAt: now,
             started: false,
@@ -151,51 +128,39 @@ contract Project is ParticipantManager {
             finalized: false,
             reward: reward
             });
-        sprints.push(newSprintMemory);
-
-        //if newSprint is not a first one, copy shares from previous;
-        if(sprints.length > 1) {
-            uint lastIndex = sprints.length - 1;
-            copySprintShares(lastIndex - 1, lastIndex);
-            //making equal shares
-        } else {
-            equalShares(sprints.length - 1);
-        }
+        sprints.push(newSprint);
     }
 
     // possible to set share only for last sprint
-    function createSetShareRequest(uint sprintIndex, uint[] shares)
-    public onlyParticipant {
-        require(sprintIndex < participantAmount);
-        require(shares.length == participantAmount);
+    function createShareRequest(
+        uint sprintIndex,
+        address[] shareHolders,
+        uint[] shares
+    ) public onlyParticipant {
+
+        require(sprintIndex < sprints.length);
+        require(shareHolders.length == shares.length);
+
         uint totalShare;
         uint i;
         for(i = 0; i< shares.length; i++) {
             totalShare+=shares[i];
         }
         require(totalShare <= 100);
-        ParticipantShare[] memory pShareArr;
-        SetShareRequest memory newRequestMemory = SetShareRequest({
+
+        ShareRequest memory newRequest = ShareRequest({
             sprintIndex: sprintIndex,
             approvalAmount: 0,
             finalized: false,
-            participantShares: pShareArr
+            shareHolders: shareHolders,
+            shares: shares
             });
         shareRequests.push(newRequest);
-        SetShareRequest storage newRequest = shareRequests[shareRequests.length-1];
-        ParticipantShare memory participantShare;
-        for(i = 0; i < shares.length; i++) {
-            participantShare = ParticipantShare({
-                participant: participantArray[i],
-                share: shares[i]
-                });
-            newRequest.participantShares.push(participantShare);
-        }
     }
 
     function approveShareRequest(uint shareRequestIndex) public onlyParticipant {
         require(shareRequestIndex < shareRequests.length);
-        SetShareRequest storage shareRequest = shareRequests[shareRequestIndex];
+        ShareRequest storage shareRequest = shareRequests[shareRequestIndex];
         require(!shareRequest.approved[msg.sender]);
 
         shareRequest.approvalAmount++;
@@ -203,43 +168,16 @@ contract Project is ParticipantManager {
     }
 
     function finalizeShareRequest(uint shareRequestIndex) public {
-        SetShareRequest storage shareRequest = shareRequests[shareRequestIndex];
+        ShareRequest storage shareRequest = shareRequests[shareRequestIndex];
         require((shareRequest.approvalAmount / participantAmount) * 100 > 50);
         Sprint storage sprint = sprints[shareRequest.sprintIndex];
-        for(uint i; i < shareRequest.participantShares.length; i++) {
-            ParticipantShare storage pShare = shareRequest.participantShares[i];
-            sprint.shares[pShare.participant] = pShare.share;
+        for(uint i; i < shareRequest.shareHolders.length; i++) {
+            sprint.shares[shareRequest.shareHolders[i]] =
+            shareRequest.shares[i];
         }
     }
 
     function getBalance() public view returns(uint) {
         return address(this).balance;
-    }
-
-    /// ---------------------------------------------
-    /// -------------------INTERNAL------------------
-    /// ---------------------------------------------
-
-    function copySprintShares(uint fromSprintIndex, uint toSprintIndex) internal {
-        Sprint storage fromSprint = sprints[fromSprintIndex];
-        Sprint storage toSprint = sprints[toSprintIndex];
-        address participant;
-
-        for(uint i = 0; i < participantArray.length; i++) {
-            participant = participantArray[i];
-            toSprint.shares[participant] = fromSprint.shares[participant];
-        }
-    }
-
-    function equalShares(uint sprintIndex) internal {
-        Sprint storage sprint = sprints[sprintIndex];
-        address participant;
-        uint equalShare = 100 / participantAmount;
-        uint remainder = 100 - participantAmount * equalShare;
-        for(uint i = 0; i < participantArray.length; i++) {
-            participant = participantArray[i];
-            sprint.shares[participant] = equalShare;
-        }
-        sprint.shares[participant] += remainder;
     }
 }
